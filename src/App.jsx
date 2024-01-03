36c3e626889df20580a5ddd6ac4f54e2c025af24
Juan Ludevid <jmludevid@hotmail.com>
2024-01-03 16:30:42 +1100

    update Login component

--- src/App.jsx
+++ src/App.jsx
@@ -11,32 +11,43 @@ import City from "./components/City";
 import Form from "./components/Form";
 import CountryList from "./components/CountryList";
 import { CitiesProvider } from "./contexts/CitiesContext";
+import ProtectedRoute from "./components/ProtectedRoute";
+import { AuthProvider } from "./contexts/FakeAuthContext";
 
 function App() {
   return (
-    <CitiesProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route index element={<HomePage />}></Route>
-          <Route path="app" element={<AppLayout />}>
-            <Route index element={<Navigate replace to="cities" />}></Route>
-            <Route index element={<CityList />}></Route>
-            <Route path="cities/:id" element={<City />}></Route>
-            <Route path="cities" element={<CityList />}></Route>
-            <Route path="countries" element={<CountryList />}></Route>
-            <Route path="form" element={<Form />}></Route>
-          </Route>
-          <Route
-            path="login"
-            element={<Login />}
-            className={styles.ctaLink}
-          ></Route>
-          <Route path="pricing" element={<Pricing />}></Route>
-          <Route path="product" element={<Product />}></Route>
-          <Route path="*" element={<PageNotFound />}></Route>
-        </Routes>
-      </BrowserRouter>
-    </CitiesProvider>
+    <AuthProvider>
+      <CitiesProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route index element={<HomePage />}></Route>
+            <Route
+              path="app"
+              element={
+                <ProtectedRoute>
+                  <AppLayout />
+                </ProtectedRoute>
+              }
+            >
+              <Route index element={<Navigate replace to="cities" />}></Route>
+              <Route index element={<CityList />}></Route>
+              <Route path="cities/:id" element={<City />}></Route>
+              <Route path="cities" element={<CityList />}></Route>
+              <Route path="countries" element={<CountryList />}></Route>
+              <Route path="form" element={<Form />}></Route>
+            </Route>
+            <Route
+              path="login"
+              element={<Login />}
+              className={styles.ctaLink}
+            ></Route>
+            <Route path="pricing" element={<Pricing />}></Route>
+            <Route path="product" element={<Product />}></Route>
+            <Route path="*" element={<PageNotFound />}></Route>
+          </Routes>
+        </BrowserRouter>
+      </CitiesProvider>
+    </AuthProvider>
   );
 }
 
